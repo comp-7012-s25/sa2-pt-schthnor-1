6d3181c684fbc3ac888c22d4f443735c839233c6
schthnor <schthnor@memphis.edu>
2025-02-06 23:33:55 -0600

    Completed SA2-PT Practice Test

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+ 
+    def welcome
+      render :welcome
+    end
+    def me
+      render :me
+    end
+   
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  root to: redirect('/welcome')
+  get 'welcome', to: 'pages#welcome', as: 'welcome'
+  get 'me', to: 'pages#me', as: 'me'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
